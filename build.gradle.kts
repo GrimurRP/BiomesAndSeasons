0389f8ee1a45b8daf3a2dd02e660890d020b7a55
Aleksey Terzi <aleksey.terzi@gmail.com>
2022-09-03 19:21:59 -0600

    Remove civGradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,44 @@
-import net.civmc.civgradle.common.util.civRepo
-
 plugins {
     `java-library`
     `maven-publish`
-    id("net.civmc.civgradle.plugin") version "1.0.0-SNAPSHOT"
 }
 
-// Temporary hack:
-// Remove the root build directory
 gradle.buildFinished {
 	project.buildDir.deleteRecursively()
 }
 
 allprojects {
-	group = "com.aleksey.bs"
-	version = "1.0.0-SNAPSHOT"
-	description = "BiomesAndSeasons"
+	group = rootProject.group
+	version = rootProject.version
+	description = rootProject.description
 }
 
 subprojects {
-	apply(plugin = "net.civmc.civgradle.plugin")
 	apply(plugin = "java-library")
 	apply(plugin = "maven-publish")
 
 	java {
-		toolchain {
-			languageVersion.set(JavaLanguageVersion.of(17))
+		toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+		withJavadocJar()
+		withSourcesJar()
+	}
+
+	tasks {
+		compileJava {
+			options.encoding = Charsets.UTF_8.name()
+			options.release.set(17)
+		}
+		processResources {
+			filteringCharset = Charsets.UTF_8.name()
+			filesMatching("**/plugin.yml") {
+				expand( project.properties )
+			}
 		}
 	}
 
 	repositories {
 		mavenCentral()
-
-		civRepo("CivMC/CivModCore")
+		maven("https://nexus.civunion.com/repository/maven-public/")
 
 		flatDir {
 			dirs("../libs")
@@ -42,16 +48,15 @@ subprojects {
 	publishing {
 		repositories {
 			maven {
-				name = "GitHubPackages"
-				url = uri("https://maven.pkg.github.com/CivMC/BiomesAndSeasons")
+				url = uri("https://nexus.civunion.com/repository/maven-releases/")
 				credentials {
-					username = System.getenv("GITHUB_ACTOR")
-					password = System.getenv("GITHUB_TOKEN")
+					username = System.getenv("REPO_USERNAME")
+					password = System.getenv("REPO_PASSWORD")
 				}
 			}
 		}
 		publications {
-			register<MavenPublication>("gpr") {
+			register<MavenPublication>("main") {
 				from(components["java"])
 			}
 		}
--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,19 +1,11 @@
 plugins {
-	`java-library`
-	id("net.civmc.civgradle.plugin")
-	id("io.papermc.paperweight.userdev") version "1.3.1"
-}
-
-civGradle {
-	paper {
-		pluginName = "BiomesAndSeasons"
-	}
+	id("io.papermc.paperweight.userdev") version "1.3.8"
 }
 
 dependencies {
 	paperDevBundle("1.18.2-R0.1-SNAPSHOT")
 
-	compileOnly("net.civmc.civmodcore:paper:2.0.0-SNAPSHOT:dev-all")
-	compileOnly("net.civmc.realisticbiomes:RealisticBiomes-paper:3.0.0-SNAPSHOT")
+	compileOnly("net.civmc.civmodcore:CivModCore:2.4.0:dev-all")
+	compileOnly("net.civmc.realisticbiomes:RealisticBiomes:3.3.0")
 	compileOnly("me.casperge.realisticseasons:RealisticSeasons:7.44")
 }
